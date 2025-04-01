6abe292b9de6db0805305ffb4331e2055d895950
Austin Pratt <183548723+NullAce@users.noreply.github.com>
2025-04-01 07:44:21 -0500

    Updated to v0.1.2

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='pysort',
+    version='0.1.2',
+    author='Austin Pratt',
+    author_email='183548723+NullAce@users.noreply.github.com',
+    description='A python file sorting library.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/NullAce/PySort',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
